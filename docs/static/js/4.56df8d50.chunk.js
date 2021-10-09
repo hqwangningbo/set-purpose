87bf8e412878efbfa87d82e5777c88971689ae37
wangningbo <2536935847@qq.com>
2021-10-09 19:41:40 +0800

    add loading

--- docs/static/js/4.56df8d50.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@scaffold-eth/react-app"]=this["webpackJsonp@scaffold-eth/react-app"]||[]).push([[4],{1441:function(p){p.exports=JSON.parse("{}")}}]);
-//# sourceMappingURL=4.56df8d50.chunk.js.map
